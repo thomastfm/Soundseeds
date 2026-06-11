1a6f93577e1dc0a8168f72720b7d1b3c1e2e598e
thomastfm <1thomaswright@gmail.com>
2026-06-10 20:52:48 -0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = "PASTE_YOUR_URL_HERE";
-const SUPABASE_KEY = "PASTE_YOUR_ANON_KEY_HERE";
+const SUPABASE_URL = "https://rduvfdwubraehqqwiupe.supabase.co";
+const SUPABASE_KEY = "sb_publishable_Qq4GuY7BqdnKgE9L1nP6Ng_Ha24s73C";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
